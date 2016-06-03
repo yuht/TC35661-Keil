e93a990554f7fd6b479578a47e1532521701faaf
yuht <yuht@qq.com>
2016-06-03 14:23:13 +0800

    demo c_bt_send()

--- USER/main.c
+++ USER/main.c
@@ -13,6 +13,8 @@
 
 int main ( void )
 {
+//	uint8_t static active= 0;
+	uint8_t static ledstat;
 	SystemInit();//系统时钟等初始化
 	if(SysTick_Config(72000*1))	 //配置错误返回1,max 16777216   默认72Mhz 时钟 ,1ms延时
 	{							
@@ -49,8 +51,13 @@ int main ( void )
 //	LCD_WriteString_en ( 3, 3, PSTR("gsm starting") );
 //	LCD_WriteString_en ( 4, 4, PSTR("gsm starting") );
 //	Timerx_Init ( 1000, 7199 ); //10Khz的计数频率，1000次为10ms
-
-
+	
+	C_bt_init();
+	C_bt_module_reset();
+	C_bt_module_init();
+	
+	
+	GSM_RST = 0; //启动GSM模块
 	LED1_ON;
 	LED2_ON;
 	LED3_ON;
@@ -61,30 +68,45 @@ int main ( void )
 	LED3_OFF;
 	LED4_OFF;
 	
-//	GSM_RST = 0; //启动GSM模块
 
-	C_bt_init();
-	C_bt_module_reset();
-	C_bt_module_init();
 
+
+	
+	GSM_RST = 1; //启动GSM模块
 	while(1)
 	{
-		LED1_ON;
-		delay_ms(500);
-		LED1_OFF;
-		delay_ms(500);
+		if( ms_delay_counter >500 ){
+			ms_delay_counter = 0;
+			if( ledstat ){
+				LED1_ON;
+				if(C_bt_device_active() ){
+					C_bt_send("test str");
+				}
+			}else{
+				LED1_OFF;
+			}
+			ledstat = !ledstat;
+		}
 		
-		if(Uart1_RcvCnt){
-			Uart1SendStr(Uart1_Buff);
-			Uart1_RcvCnt = 0;
-		}
-		if(Uart2_RcvCnt){
-			Uart1SendStr(Uart2_Buff);
-			Uart2_RcvCnt = 0;
-		}
-		if(Uart3_RcvCnt){
-			Uart1SendStr(Uart3_Buff);
-			Uart3_RcvCnt = 0;
-		}
+		C_bt_step();
+		
+
+//		LED1_ON;
+//		delay_ms(500);
+//		LED1_OFF;
+//		delay_ms(500);
+		
+//		if(Uart1_RcvCnt){
+//			Uart1SendStr(Uart1_Buff);
+//			Uart1_RcvCnt = 0;
+//		}
+//		if(Uart2_RcvCnt){
+//			Uart1SendStr(Uart2_Buff);
+//			Uart2_RcvCnt = 0;
+//		}
+//		if(Uart3_RcvCnt){
+//			Uart1SendStr(Uart3_Buff);
+//			Uart3_RcvCnt = 0;
+//		}
 	}
 }
